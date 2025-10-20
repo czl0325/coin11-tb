b27d115b61b055a3a1e6e72c19a231d59a9758cd
czl <295183917@qq.com>
2025-10-20 19:28:24 +0800

    2025双十一做一半

---
+++ 2025淘宝双十一.py
@@ -0,0 +1,119 @@
+import time
+import random
+
+import uiautomator2 as u2
+from uiautomator2 import Direction
+from utils import check_chars_exist, other_app, get_current_app
+
+d = u2.connect()
+d.app_start("com.taobao.taobao", stop=True, use_monkey=True)
+screen_width = d.info['displayWidth']
+screen_height = d.info['displayHeight']
+time.sleep(5)
+in_search = False
+in_other_app = False
+have_clicked = []
+unclick_btn = []
+
+ctx = d.watch_context()
+ctx.when("O1CN012qVB9n1tvZ8ATEQGu_!!6000000005964-2-tps-144-144").click()
+ctx.when("O1CN01TkBa3v1zgLfbNmfp7_!!6000000006743-2-tps-72-72").click()
+ctx.when(xpath="//android.app.Dialog//android.widget.Button[@text='关闭']").click()
+ctx.when(xpath="//android.widget.TextView[@package='com.eg.android.AlipayGphone']").click()
+ctx.start()
+
+def check_in_task():
+    package_name, activity_name = get_current_app(d)
+    if package_name == "com.taobao.taobao" and activity_name == "com.taobao.themis.container.app.TMSActivity":
+        phy_view = d(className="android.widget.TextView", text="做任务赚体力")
+        if phy_view.exists:
+            return True
+    return False
+
+def operate_task():
+    start_time = time.time()
+    while True:
+        if time.time() - start_time > 18:
+            break
+        start_x = random.randint(screen_width // 6, screen_width // 2)
+        start_y = random.randint(screen_height // 2, screen_height - screen_width // 4)
+        end_x = random.randint(start_x - 100, start_x)
+        end_y = random.randint(start_y - 1200, start_y - 300)
+        swipe_time = random.uniform(0.4, 1) if end_y - start_y > 500 else random.uniform(0.2, 0.5)
+        print("模拟滑动", start_x, start_y, end_x, end_y, swipe_time)
+        d.swipe(start_x, start_y, end_x, end_y, swipe_time)
+        time.sleep(random.uniform(1, 5))
+    print("开始返回界面")
+    while True:
+        if check_in_task():
+            print("当前是任务列表画面，不能继续返回")
+            break
+        else:
+            package_name, activity_name = get_current_app(d)
+            print(f"{package_name}--{activity_name}")
+            if package_name != "com.taobao.taobao":
+                d.app_start("com.taobao.taobao", stop=False)
+            else:
+                d.press("back")
+                time.sleep(0.5)
+
+while True:
+    package_name, activity_name = get_current_app(d)
+    if package_name == "com.taobao.taobao" and activity_name != "com.taobao.tao.welcome.Welcome":
+        break
+    coin_btn = d(className="android.view.View", description="领淘金币")
+    if coin_btn.exists:
+        coin_btn.click()
+    else:
+        raise Exception("没有找到领淘金币按钮")
+    time.sleep(4)
+time.sleep(10)
+print("进入领淘金币按钮")
+physical_btn = d(className="android.widget.Button", text="赚体力")
+if physical_btn.exists:
+    physical_btn.click()
+    time.sleep(5)
+finish_count = 0
+time1 = time.time()
+while True:
+    to_btn = d(className="android.widget.Button", text="去完成")
+    if to_btn.exists:
+        need_click_view = None
+        need_click_index = 0
+        task_name = None
+        for index, view in enumerate(to_btn):
+            text_div = view.sibling(className="android.view.View", instance=0).child(className="android.widget.TextView", instance=0)
+            if text_div.exists:
+                if check_chars_exist(text_div.get_text()):
+                    if view not in unclick_btn:
+                        unclick_btn.append(view)
+                    continue
+                task_name = text_div.get_text()
+                if task_name in have_clicked:
+                    continue
+                need_click_index = index
+                need_click_view = view
+                break
+        if need_click_view:
+            print("点击按钮", task_name)
+            if task_name not in have_clicked:
+                have_clicked.append(task_name)
+            need_click_view.click()
+            time.sleep(2)
+            search_view = d(className="android.view.View", text="搜索有福利")
+            if search_view.exists:
+                d(className="android.widget.EditText", instance=0).send_keys("笔记本电脑")
+                d(className="android.widget.Button", text="搜索").click()
+                in_search = True
+                time.sleep(2)
+            operate_task()
+        else:
+            break
+    time.sleep(4)
+d.watcher.remove()
+print(f"共自动化完成{finish_count}个任务")
+d.shell("settings put system accelerometer_rotation 0")
+print("关闭手机自动旋转")
+time2 = time.time()
+minutes, seconds = divmod(int(time2 - time1), 60)  # 同时计算分钟和秒
+print(f"共耗时: {minutes} 分钟 {seconds} 秒")
