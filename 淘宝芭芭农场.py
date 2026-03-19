517e9e981eae9c1a1d830247ee0ca447e2d4621f
陈昭良 <295183917@qq.com>
2026-03-19 16:43:18 +0800

    小程序退出处理

--- 淘宝芭芭农场.py
+++ 淘宝芭芭农场.py
@@ -56,6 +56,12 @@ def back_to_task():
                     close_btn1.click()
                     time.sleep(1)
                     continue
+                close_btn2 = d(className="android.widget.TextView", resourceId="com.taobao.taobao:id/back_home_btn")
+                if close_btn2.exists:
+                    print("点击关闭小程序按钮")
+                    close_btn2.click()
+                    time.sleep(1)
+                    continue
                 task_view = d.xpath('//android.widget.TextView[contains(@text, "限时下单任务")]')
                 if task_view.exists:
                     close_btn2 = d.xpath('//android.widget.TextView[contains(@text, "限时下单任务")]/preceding-sibling::android.view.View[1]')
